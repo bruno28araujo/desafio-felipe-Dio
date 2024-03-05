20048b99e0311ad5afbec8a6424db8ae4e4d0ddb
bruno28araujo <bruno28araujo@gmail.com>
2024-03-05 08:57:45 -0300

    projeto com a ultima atualização

--- index.js
+++ index.js
@@ -1,47 +1,47 @@
-let nome = "Bruno"
-let xp = 3086
+let nome = "Super Cão"
+let xp = 1
 let nivel = ""
 
 if (xp < 1000 ){
-        nivel = "Ferrou"
-        console.log(nivel + " Se esforce mais.")
-        console.log("Quantidade de XPs: " + xp)
+        nivel = "Ferrou!"
+        console.log(nivel + " Se esforce mais " + nome + ".")
+        console.log("Você possui: " + xp + " XPs.")
 }
 else if(xp >= 1001 && xp <= 2000){
     nivel = "Bronze"
-    console.log("Seu nível atual é: " + nivel )
-    console.log("Quantidade de XPs: " + xp)
+    console.log(nome + ", seu nível atual é: " + nivel + ".")
+    console.log("Você possui: " + xp + " XPs.")
 }
 else if(xp >= 2001 && xp <= 5000){
     nivel = "Prata"
-    console.log("Seu nível atual é: " + nivel )
-    console.log("Quantidade de XPs: " + xp)
+    console.log(nome + ", seu nível atual é: " + nivel + ".")
+    console.log("Você possui: " + xp + " XPs.")
 }
 else if(xp >= 5001 && xp <= 7000){
     nivel = "Ouro"
-    console.log("Seu nível atual é: " + nivel )
-    console.log("Quantidade de XPs: " + xp)
+    console.log(nome + ", seu nível atual é: " + nivel + ".")
+    console.log("Você possui: " + xp + " XPs.")
 }
 else if(xp >= 7001 && xp <= 8000){
     nivel = "Platina"
-    console.log("Seu nível atual é: " + nivel )
-    console.log("Quantidade de XPs: " + xp)
+    console.log(nome + ", seu nível atual é: " + nivel + ".")
+    console.log("Você possui: " + xp + " XPs.")
 }
 else if(xp >= 8001 && xp <= 9000){
     nivel = "Ascendente"
-    console.log("Seu nível atual é: " + nivel )
-    console.log("Quantidade de XPs: " + xp)
+    console.log(nome + ", seu nível atual é: " + nivel + ".")
+    console.log("Você possui: " + xp + " XPs.")
 }
 else if(xp >= 9001 && xp <= 10000){
     nivel = "imortal"
-    console.log("Seu nível atual é: " + nivel )
-    console.log("Quantidade de XPs: " + xp)
+    console.log(nome + ", seu nível atual é: " + nivel + ".")
+    console.log("Você possui: " + xp + " XPs.")
 }
 else if(xp >= 10001){
     nivel = "Radiante"
-    console.log("Seu nível atual é: " + nivel )
-    console.log("Quantidade de XPs: " + xp)
+    console.log(nome + ", seu nível atual é: " + nivel + ".")
+    console.log("Você possui: " + xp + " XPs.")
 }
 else{
-    console.log("Erro de entradas!")
+    console.log("Erro de entradas! Tente novamente.")
 }
